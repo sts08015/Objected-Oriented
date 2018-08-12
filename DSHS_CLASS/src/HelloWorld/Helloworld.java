afab1c2b4058bffb2d159324ef984fcea0cede86
FisMA <31784008+sts08015@users.noreply.github.com>
2018-08-13 08:48:44 +0900

    첫수업
    객체지향 첫수업

---
+++ DSHS_CLASS/src/HelloWorld/Helloworld.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class Helloworld {
+	String txt;
+	
+	Helloworld(String txt){
+		this.txt = txt;
+	}
+	
+	void printM() {
+		System.out.print(txt);
+	}
+}
---
+++ DSHS_CLASS/src/HelloWorld/HelloworldTest.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+public class HelloworldTest {
+	
+	public static void main(String[] args) {
+		Helloworld hw = new Helloworld("¾È³ç ¼¼°è¾ß?");
+		hw.printM();
+	}
+	
+}
