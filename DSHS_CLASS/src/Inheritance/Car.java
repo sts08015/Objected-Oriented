583245dcfd7d1eaebc4c2cfb0801a686883562e9
FisMA <31784008+sts08015@users.noreply.github.com>
2018-10-01 15:07:31 +0900

    oop

---
+++ DSHS_CLASS/src/Inheritance/Car.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class Car {
+	public int speed;
+	public void speedUp() {speed +=1;}
+	public final void stop() {
+		System.out.println("Â÷¸¦ ¸ØÃã");
+		speed = 0;
+	}
+}
---
+++ DSHS_CLASS/src/Inheritance/SportsCar.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class SportsCar extends Car{
+	@Override
+	public void speedUp() {speed +=10;}
+	/*
+	 final 붙은 것은 못한다.
+	 */	
+}
