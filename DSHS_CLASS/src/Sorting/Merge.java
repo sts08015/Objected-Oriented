4d718e2b9ba03928dc8e8a4f525148e61854e0d9
FisMA <31784008+sts08015@users.noreply.github.com>
2018-10-03 14:06:26 +0900

    let's merge

--- DSHS_CLASS/src/Sorting/Merge.java
+++ DSHS_CLASS/src/Sorting/Merge.java
@@ -8,27 +8,26 @@ public class Merge {
 	void divide(int[] a,int[] b,int low,int high) {
 		if(high<=low) return;
 		int mid = (low+high)/2;
-		divide(a,b,0,mid);
+		divide(a,b,low,mid);
 		divide(a,b,mid+1,high);
 		merge(a,b,low,mid,high);
 	}
-	void merge(int[] a,int[] b,int low,int mid,int high) {
+	void merge(int[] a,int [] b,int low,int mid,int high) {
 		int i = low;
 		int j = mid+1;
 		for(int k = low;k<=high;k++) {
 			if(i>mid) b[k] = a[j++];
 			else if(j>high) b[k] = a[i++];
-			else if(a[i] < a[j]) b[k] = a[i++];
-			else b[k] = a[j++];
+			else if(a[i] > a[j]) b[k] = a[j++];
+			else b[k] = a[i++];
 		}
 		for(int k = low;k<=high;k++) {
 			a[k] = b[k];
 		}
-		
 	}
 	public static void main(String[] args) {
-		int[] arr = {1,315,52,6,3,67,232};
 		Merge m = new Merge();
+		int[] arr = {1,2,23135,5246,223421,6211,134};
 		m.sort(arr);
 		for(int i: arr) {
 			System.out.println(i);
