1ea9c62099133958fdd167d2770df9b432962f4e
FisMA <31784008+sts08015@users.noreply.github.com>
2018-10-05 14:28:13 +0900

    dd

--- DSHS_CLASS/src/Sorting/Heap.java
+++ DSHS_CLASS/src/Sorting/Heap.java
@@ -8,24 +8,21 @@ public class Heap {
 	}
 	void sort(int[] a) {
 		int N = a.length-1;
-		for(int i = N/2;i>0;i--) {
-			downheap(a,i,N);
-		}
-		while(N>0) {
+		for(int i = N/2;i>0;i--) downheap(a,i,N);
+		while(N>1) {
 			swap(a,1,N--);
 			downheap(a,1,N);
 		}
 	}
-	void downheap(int[]a,int p,int heapsize) {
-		while(2*p<=heapsize) {
+	void downheap(int[] a,int p,int heapsize) {
+		while(2*p <= heapsize) {
 			int s = 2*p;
-			if(a[p]>a[s]) break;
-			if(s<heapsize && a[s] < a[s+1]) s++;
-			swap(a,s,p);
+			if(a[s] < a[p]) break;
+			if(s<heapsize && a[s]<a[s+1]) s++;
+			swap(a,p,s);
 			p = s;
 		}
 	}
-	
 	public static void main(String[] args) {
 		int[] arr = {0x7fffffff,1,4,5,9,8,3,2,7,6};
 		Heap h = new Heap();
