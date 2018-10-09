97b9afa69ab071adc228e090a74f6b7e009e4f4e
FisMA <31784008+sts08015@users.noreply.github.com>
2018-10-09 16:45:00 +0900

    test

---
+++ DSHS_CLASS/src/Chapter_7/ChildExample.java
@@ -0,0 +1,15 @@
+package Chapter_7;
+
+public class ChildExample {
+	public static void main(String[] args) {
+		Parent parent = new Child();
+		parent.field1 = "data1";
+		parent.method1();
+		parent.method2();
+		
+		Child child = (Child) parent;
+		child.field2 = "yyy";
+		child.method3();
+		System.out.println(child.field1);
+	}
+}
