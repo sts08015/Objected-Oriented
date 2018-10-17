ee65b9444ef93c37499fe9a585bd701ec0d96cfa
FisMA <31784008+sts08015@users.noreply.github.com>
2018-10-17 12:11:34 +0900

    test over

--- DSHS_CLASS/src/Test/Test.java
+++ DSHS_CLASS/src/Test/Test.java
@@ -1,26 +1,11 @@
 package Test;
 
 public class Test {
-	static Student st = new Student("Emma");
-	public static void main(String[] args) {
-		Student st = new Student("Vera");
-		Test ts = new Test();
-		ts.st = st;
-		ts.st.change(st);
-		System.out.print(st.name + ", ");
-		System.out.print(new Test().st.name);
-		String a = "aaa";
-		String b = "aaa";
-		System.out.println(a==b);
-	}
+	int iv = 10;
+	static int cv = 20;
+	int iv2 = cv;
+	static int cv2 = iv;
+	
+	
 
-}
-class Student{
-	String name;
-	public Student(String name) {
-		this.name = name;
-	}
-	public void change(Student student) {
-		student = new Student("David");
-	}
 }
