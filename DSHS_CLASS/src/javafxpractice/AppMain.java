08f1d556c4c084a552a1e692f71cf7299974ef17
FisMA <31784008+sts08015@users.noreply.github.com>
2018-10-25 21:14:23 +0900

    d

---
+++ DSHS_CLASS/src/javafxpractice/AppMain.java
@@ -0,0 +1,11 @@
+package javafxpractice;
+
+public class AppMain extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
