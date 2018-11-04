d387b8c3c6bd0d51f41cf6090bf03e3521a58266
FisMA <31784008+sts08015@users.noreply.github.com>
2018-11-05 08:36:53 +0900

    dadf

---
+++ DSHS_CLASS/src/javafxpractice/App.java
@@ -0,0 +1,12 @@
+package javafxpractice;
+
+import javafx.stage.Stage;
+
+public class App {
+	public void start(Stage primaryStage) throws Exception{
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
